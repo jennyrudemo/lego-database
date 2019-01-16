44495c8cadeb124e9bdfa23738cc53d7fae61c60
jennyrudemo <36629887+jennyrudemo@users.noreply.github.com>
2019-01-16 15:12:43 +0100

    Update search.php
    Fix för att pagination ska fungera

--- search.php
+++ search.php
@@ -7,15 +7,17 @@
 		
 		$search = ' WHERE (sets.SetID LIKE "'.$sequered.'%" OR sets.Setname LIKE "%'.$sequered.'%") ';
 		
+		
 	if(isset($_GET["search"]))
 	{
-		echo "din sökning:".$_GET['search'];
+		
 		
 		//$test = "sets.SetID LIKE '%".$_GET["search"]."%'";//Vi vill söka på setID, men i start vill vi inte ha alla.
 		//$test_name="sets.Setname LIKE '%".$_GET["search"]."%'";
 		$limit = $_GET["limit"];
 		$order = mysqli_real_escape_string($link, $_GET["searchorder"]);
 		
+		$searchorder = mysqli_real_escape_string($link, $_GET["searchorder"]);
 		
 		
 		
@@ -33,56 +35,45 @@
 				
 				
 			
-			echo "rbutton = ".$_COOKIE['rbutton'];
+			
 			if(!$limit = $_GET["limit"])
 			{
-				$limit = 30;
+				$limit = 10000;
 			}
 			
-			print "GET:". $_GET["searchorder"]. "end ";
+			
 			
 			//Ställer in hur många resultat som visas per sida
 			//flytta upp denna högre upp?
 			$itemsPerPage= 10;
 			
 			if(isset($_GET['page']) && !empty($_GET['page'])){
-				$page = ($_GET['page'] - 1)*$itemsPerPage;
+				$page= $_GET['page'];
+				$offset = ($page - 1)*$itemsPerPage;
+				
+				$result = mysqli_query($link, //I FROM behöver vi sets?
+				"SELECT sets.SetID, sets.Setname, images.ItemID, images.has_gif, images.has_jpg, images.has_largejpg,
+				images.has_largegif, sets.Year
+				FROM sets, images 
+				$search AND sets.SetID = images.ItemID AND images.ItemtypeID = 'S'
+				ORDER BY $order 
+				LIMIT $limit OFFSET $offset" //Vi vill ha en offset här < så att man kan visa de andra satserna! Helst med hjälp av att klicka på en knapp elr något.
+				);
+			}
+			else{
+				$page = 1;
+				
+				$result = mysqli_query($link, //I FROM behöver vi sets?
+				"SELECT sets.SetID, sets.Setname, images.ItemID, images.has_gif, images.has_jpg, images.has_largejpg,
+				images.has_largegif, sets.Year
+				FROM sets, images 
+				$search AND sets.SetID = images.ItemID AND images.ItemtypeID = 'S'
+				ORDER BY $order 
+				LIMIT $limit" //Vi vill ha en offset här < så att man kan visa de andra satserna! Helst med hjälp av att klicka på en knapp elr något.
+				);
 			}
 			//ev ändra variabel $page till $offset
 			
-			//test
-			print("test ");
-			print("$page");
-			
-		if($page > 0){
-			$result = mysqli_query($link, //I FROM behöver vi sets?
-			"SELECT sets.SetID, sets.Setname, images.ItemID, images.has_gif, images.has_jpg, images.has_largejpg,
-			images.has_largegif, sets.Year
-			FROM sets, images 
-			$search AND sets.SetID = images.ItemID AND images.ItemtypeID = 'S'
-			ORDER BY $order 
-			LIMIT $limit OFFSET $page" //Vi vill ha en offset här < så att man kan visa de andra satserna! Helst med hjälp av att klicka på en knapp elr något.
-			);
-			
-			print("HEJ!");
-		}
-		else{
-			$result = mysqli_query($link, //I FROM behöver vi sets?
-			"SELECT sets.SetID, sets.Setname, images.ItemID, images.has_gif, images.has_jpg, images.has_largejpg,
-			images.has_largegif, sets.Year
-			FROM sets, images 
-			$search AND sets.SetID = images.ItemID AND images.ItemtypeID = 'S'
-			ORDER BY $order 
-			LIMIT $limit" //Vi vill ha en offset här < så att man kan visa de andra satserna! Helst med hjälp av att klicka på en knapp elr något.
-			);
-			
-			//test
-			print("else.... ");
-		}
-		print "NEXT:". $_GET["next_page"];
-		print "GET:". $sequered;	
-		print "GET:". $_GET["search"];
-		
 	
 	}
 	else{
