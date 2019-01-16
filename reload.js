94007441b7a9cc0e21c999499c02ad2f1b1dfbd9
xang23 <46744878+xang23@users.noreply.github.com>
2019-01-16 12:01:53 +0100

    Update reload.js

--- reload.js
+++ reload.js
@@ -3,4 +3,19 @@ window.onload = function() {
 				window.location = window.location + '#reload';
 				window.location.reload();
 				}
-				}
+			}
+
+function help_window() {
+	var div = document.getElementById("help_box");
+	var blur = document.getElementById("container");
+	 if (div.style.display =="block"){
+		div.style.display = "none";
+		blur.style.backgroundColor = "rgba(0,0,0,0)";
+		blur.style.filter = "blur(0px)" ;
+	}else{
+		div.style.display = "block";
+		blur.style.backgroundColor = "rgba(0,0,0, 0)";
+		blur.style.filter = "blur(6px)";
+
+	}
+}
